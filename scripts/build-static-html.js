05f99d43192bf8296a9a393c0a0f22e5b1c38e28
MarvinMichel <mmhsernee@gmail.com>
2021-03-20 14:51:03 +0100

    Add html static prerendering

---
+++ scripts/build-static-html.js
@@ -0,0 +1,60 @@
+const fs = require('fs')
+const dotenv = require('dotenv')
+const ejs = require('ejs')
+const fetch = require('node-fetch')
+const { join } = require('path')
+
+const fsPromises = fs.promises
+dotenv.config()
+
+async function getImageData(endpoint = 'photos', param) {
+  const apiURL = process.env.API_URL
+  const apiKey = process.env.API_KEY
+  const fetchURL = `${apiURL}${endpoint}?client_id=${apiKey}&per_page=35${param ? `${param}` : ''}`
+  
+  try {
+    const response = await fetch(fetchURL)
+    let data = await response.json()
+    if (endpoint === 'search/photos') data = data.results
+    return data
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+(async function buildHome() {
+  const images = await getImageData()
+  const renderData = {
+    title: 'Feed',
+    images: images
+  }
+
+  const html = renderHTML('./components/pages/index.ejs', renderData)
+  createFile('./public', 'index.html', html)
+})()
+
+/**
+ * @param {String} path relative path to view template
+ * @param {Object} data image data
+ * @returns template string with HTML
+ */
+function renderHTML(path, data) {
+  const template = fs.readFileSync(path, 'utf-8').toString()
+  return ejs.render(template, data, {
+    views: [join(__dirname, '../', 'components' ,'pages')]
+  })
+}
+
+/**
+ * * { recursive: true } parent folders should be created
+ * @param {String} dir relative path to file directory
+ * @param {String} name name of the file
+ * @param {String} content HTML template
+ * @returns 
+ */
+function createFile(dir, name, content) {
+  return fsPromises.mkdir(dir, { recursive: true })
+    .then(() => {
+      return fsPromises.writeFile(join(dir, name), content)
+    })
+}
