6fd1fe212446a159e5b1307b4cbc3484232ca432
MarvinMichel <mmhsernee@gmail.com>
2021-03-28 16:58:24 +0200

    Make elements global scoped

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 // Modules
 import { resizeGridItems } from './modules/resizeGridItems.js'
 import { setActiveTab } from './modules/setActiveTab.js'
-import 'lazysizes'
 
 // Styling
 import './styles/style.css'
 
-window.addEventListener('load', () => {
+// window.addEventListener('load', () => {
+//   if ('serviceWorker' in navigator) {
+//     navigator.serviceWorker
+//       .register('/serviceWorker.js', { scope: '/' })
+//       .then(res => console.info('service worker registered at', res.scope))
+//       .catch(err => console.err('service worker not registered', err))
+//   }
+// })
+
+window.addEventListener('DOMContentLoaded', () => {
   if (document.querySelector('.feed')) {
     setActiveTab()
 
@@ -14,12 +22,24 @@ window.addEventListener('load', () => {
       resizeGridItems()
       window.addEventListener('resize', resizeGridItems)
     }
-  }
 
-  // if ('serviceWorker' in navigator) {
-  //   navigator.serviceWorker
-  //     .register('/serviceWorker.js', { scope: '/' })
-  //     .then(res => console.info('service worker registered at', res.scope))
-  //     .catch(err => console.err('service worker not registered', err))
-  // }
+    if ('IntersectionObserver' in window) {
+      const lazyloadImages = document.querySelectorAll('.lazy')
+      const imageObserver = new IntersectionObserver(entries => {
+        entries.forEach((entry, index) => {
+          if (entry.isIntersecting) {
+            console.log(index, ': I\'m into the viewport!')
+            const image = entry.target
+            image.src = image.dataset.src
+            image.classList.remove('lazy')
+            imageObserver.unobserve(image)
+          }
+        })
+      })
+
+      lazyloadImages.forEach(image => {
+        imageObserver.observe(image)
+      })
+    }
+  }
 })
--- src/modules/resizeGridItems.js
+++ src/modules/resizeGridItems.js
@@ -1,28 +1,30 @@
 import imagesLoaded from 'imagesloaded'
 
+const grid = document.querySelector('.feed')
+const gridCells = document.querySelectorAll('.feed--image')
+
+/**
+ * Dynamically set row-span from image container
+ * @param {Element} item cell from the masonry grid
+ */
 function resizeGridItem(item) {
-  /**
-   * Resize grid-item's height based rowHeight
-   * @const rowSpan how many rows to take in grid
-  */
-  const grid = document.querySelector('.feed')
   const rowHeight = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-auto-rows'))
   const rowGap = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-row-gap'))
-  const itemHeight = item.querySelector('.feed--image__photo').getBoundingClientRect().height
+  const itemHeight = item.querySelector('.content-wrapper').getBoundingClientRect().height
   const rowSpan = Math.ceil((itemHeight + rowGap) / (rowHeight + rowGap))
   item.style.gridRowEnd = `span ${rowSpan}`
 }
 
 // Loop trough multiple items
 function resizeGridItems() {
-  const gridCells = document.querySelectorAll('.feed--image')
   gridCells.forEach(gridCell => {
     imagesLoaded(gridCell, resizeInstance)
+    grid.classList.add('loaded')
   })
 }
 
 // Resize grid item after image is fully loaded
-function resizeInstance(instance){
+function resizeInstance(instance) {
   const item = instance.elements[0]
   resizeGridItem(item)
 }
